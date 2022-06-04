59fd1ac792514b07f03ae8cc30008d6db8b5b72a
Bkirkb <b.kirkby489@gmail.com>
2022-06-04 17:55:59 +0100

    Null checking

--- csharp/src/ReversingString/StringUtilities.cs
+++ csharp/src/ReversingString/StringUtilities.cs
@@ -2,21 +2,30 @@
 
 public class StringUtilities
 {
+    // Method can be made more durable by changing input to an object, dealing with things other than string and Null
     public static string Reverse(string s)
     {
-        char[] inputChars = s.ToCharArray();
+        
         string reversedString = string.Empty;
-        try
+        if (string.IsNullOrEmpty(s))
         {
-            for(int i = inputChars.Length - 1; i >= 0; i--)
+            return "String is null or empty";
+        }
+        else
+        {
+            char[] inputChars = s.ToCharArray();
+            try
             {
-                reversedString += inputChars[i];
+                for (int i = inputChars.Length - 1; i >= 0; i--)
+                {
+                    reversedString += inputChars[i];
+                }
             }
+            catch
+            {
+                throw new ArgumentException("Input should be a string with a length of more than 1");
+            }
+            return reversedString;
         }
-        catch
-        {
-            throw new ArgumentException("Input should be a string with a length of more than 1");
-        }
-        return reversedString;
     }
 }
--- csharp/tests/ReverseString.Tests/TestReverse.cs
+++ csharp/tests/ReverseString.Tests/TestReverse.cs
@@ -5,7 +5,7 @@ namespace CodingChallenge.ReversingString.Tests
     [TestFixture]
     public class ReversingStringTests
     {
-        //Possible test cases - string, empty string. function is always expecting a string - could change function to expect an object, so that incorrect inputs are parsed correctly.
+        //Possible test cases - string, empty/null string. function is always expecting a string - could change function to expect an object, so that incorrect inputs are parsed correctly.
         [Test]
         public void TestReversingString()
         {
@@ -13,6 +13,22 @@ namespace CodingChallenge.ReversingString.Tests
             string expctedResult = "desrever eb dluohs siht";
             string result = StringUtilities.Reverse(input);
             Assert.AreEqual(result, expctedResult);
+        }
+        [Test]
+        public void TestReversingStringNull()
+        {
+            string input = null;
+            var expectedResult = "String is null or empty";
+            string result = StringUtilities.Reverse(input);
+            Assert.AreEqual(result, expectedResult);
+        }
+        [Test]
+        public void TestReversingStringOne()
+        {
+            string input = "e";
+            string expectedResult = "e";
+            string result = StringUtilities.Reverse(input);
+            Assert.AreEqual(result, expectedResult);
         }
     }
 }
